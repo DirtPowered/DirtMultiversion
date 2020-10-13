ff196c56f75b0a27ec01fa6c117b00fee7136170
JSDev <66041584+jsdevv@users.noreply.github.com>
2020-10-13 17:00:45 -0400

    Redirect to Configuration to make migrating easier for now

--- src/main/java/com/github/dirtpowered/dirtmv/data/Constants.java
+++ src/main/java/com/github/dirtpowered/dirtmv/data/Constants.java
@@ -22,12 +22,14 @@
 
 package com.github.dirtpowered.dirtmv.data;
 
+import com.github.dirtpowered.dirtmv.config.Configuration;
+
 public class Constants {
-    public static final MinecraftVersion REMOTE_SERVER_VERSION = MinecraftVersion.B_1_6_6;
+    public static final MinecraftVersion REMOTE_SERVER_VERSION = Configuration.serverVersion;
 
-    public static final String SERVER_HOST = "localhost";
-    public static final String REMOTE_HOST = "localhost";
+    public static final String SERVER_HOST = Configuration.serverAddress;
+    public static final String REMOTE_HOST = Configuration.proxyLocalAddress;
 
-    public static final int SERVER_PORT = 25565;
-    public static final int REMOTE_PORT = 25567;
+    public static final int SERVER_PORT = Configuration.serverPort;
+    public static final int REMOTE_PORT = Configuration.proxyLocalPort;
 }
