62b2e114bb72e06f49c074d516305dc6915ddfc0
outofmem13 <nulln785@gmail.com>
2020-11-14 14:21:46 +0100

    fix inventory issues on r1.5.1+ clients (1/2)

--- src/main/java/com/github/dirtpowered/dirtmv/network/versions/Release60To51/ProtocolRelease60To51.java
+++ src/main/java/com/github/dirtpowered/dirtmv/network/versions/Release60To51/ProtocolRelease60To51.java
@@ -34,6 +34,8 @@ import com.github.dirtpowered.dirtmv.data.translator.ServerProtocol;
 import com.github.dirtpowered.dirtmv.data.utils.PacketUtil;
 import com.github.dirtpowered.dirtmv.network.server.ServerSession;
 
+import java.io.IOException;
+
 public class ProtocolRelease60To51 extends ServerProtocol {
 
     public ProtocolRelease60To51() {
@@ -81,7 +83,7 @@ public class ProtocolRelease60To51 extends ServerProtocol {
 
             @Override
             public PacketData translate(ServerSession session, PacketData data) {
-                ItemStack[] itemArray = (ItemStack[]) data.read(1).getObject();
+                ItemStack[] itemArray = data.read(Type.V1_3R_ITEM_ARRAY, 1);
                 // TODO: Inventory cache
                 return data;
             }
@@ -92,8 +94,8 @@ public class ProtocolRelease60To51 extends ServerProtocol {
 
             @Override
             public PacketData translate(ServerSession session, PacketData data) {
-                ItemStack item = (ItemStack) data.read(2).getObject();
-                // TODO: Inventowry cache
+                ItemStack item = data.read(Type.V1_3R_ITEM, 2);
+                // TODO: Inventory cache
                 return data;
             }
         });
@@ -102,9 +104,65 @@ public class ProtocolRelease60To51 extends ServerProtocol {
         addTranslator(0x66, PacketDirection.CLIENT_TO_SERVER, new PacketTranslator() {
 
             @Override
-            public PacketData translate(ServerSession session, PacketData data) {
-                // TODO: fix inventory issues
-                return data;
+            public PacketData translate(ServerSession session, PacketData data) throws IOException {
+                boolean leftClickFlag = false;
+                boolean startDragging = false;
+                boolean endDragging = false;
+                boolean droppingUsingQ = false;
+                boolean addSlot = false;
+
+                short slot = data.read(Type.SHORT, 1);
+
+                byte param = data.read(Type.BYTE, 2);
+                byte inventoryAction = data.read(Type.BYTE, 4);
+
+                ItemStack item = data.read(Type.V1_3R_ITEM, 5);
+
+                switch (inventoryAction) {
+                    case 0:
+                        leftClickFlag = param == 0;
+                        break;
+                    case 4:
+                        droppingUsingQ = param + (slot != -999 ? 2 : 0) == 2;
+                        break;
+                    case 5:
+                        startDragging = param == 0;
+                        endDragging = param == 2;
+                        addSlot = param == 1;
+                        break;
+                }
+
+                boolean leftClick = leftClickFlag || startDragging || addSlot || endDragging;
+                boolean clickingOutside = slot == -999 && inventoryAction != 5;
+                boolean usingShift = inventoryAction == 1;
+
+                int mouseClick = leftClick ? 0 : 1;
+
+                if (droppingUsingQ) {
+                    PacketData closeWindow = PacketUtil.createPacket(0x65, new TypeHolder[] {
+                            set(Type.BYTE, (byte) 0)
+                    });
+
+                    session.sendPacket(closeWindow, PacketDirection.SERVER_TO_CLIENT, getFrom());
+                    return new PacketData(-1);
+                }
+
+                ItemStack cachedItem = null; // TODO: cache
+                ItemStack itemStack = item == null ? (slot < 0 ? null : cachedItem) : item;
+
+                if (itemStack == null && !clickingOutside)
+                    return new PacketData(-1);
+
+                //inventory.setLastSlot(slot);
+
+                return PacketUtil.createPacket(0x66, new TypeHolder[] {
+                        data.read(0),
+                        data.read(1),
+                        set(Type.BYTE, (byte) mouseClick),
+                        set(Type.SHORT, 0),
+                        set(Type.BYTE, (byte) (usingShift ? 1 : 0)),
+                        set(Type.V1_3R_ITEM, itemStack)
+                });
             }
         });
 
@@ -116,8 +174,8 @@ public class ProtocolRelease60To51 extends ServerProtocol {
                 Motion motion = data.read(Type.MOTION, 7);
 
                 int throwerId = motion.getThrowerId();
-
                 byte vehicleType = data.read(Type.BYTE, 1);
+
                 byte vehicleFixedType = vehicleType;
 
                 switch (vehicleType) {
